36132227f2970d881d235b318ae9cb2c45b81b84
kainat-nazir1 <65025764+kainat-nazir1@users.noreply.github.com>
2020-07-12 23:32:08 +0500

    .

---
+++ Assignment4BKainatNazir/Hummingbirds/Assets/Scripts/wait.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class wait : MonoBehaviour
+{
+   public void pengiun()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void humming_bird()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
